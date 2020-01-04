280af7e593b4e151dbeba589626ad801944c4f87
s8627213 <1061383839@qq.com>
2020-01-04 10:09:50 +0800

    category

--- src/pages/category/List.vue
+++ src/pages/category/List.vue
@@ -1,5 +1,128 @@
 <template>
-<div>
-    分类管理
-</div>
+  <div>
+    <!-- 按钮 -->
+    <el-button type="success" size="small" @click="toAddHandler">添加</el-button> 
+    <el-button type="danger" size="small">批量删除</el-button>
+    <!-- /按钮 -->
+    <!-- 表格 -->
+    <el-table :data="categorys">
+      <el-table-column prop="id" label="编号"></el-table-column>
+      <el-table-column prop="name" label="栏目管理"></el-table-column>
+      <el-table-column prop="number" label="序号"></el-table-column>
+      <el-table-column prop="parentId" label="父栏目"></el-table-column>
+      <el-table-column label="操作">
+        <template v-slot="slot">
+          <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+          <a href="" @click.prevent="toUpdateHandler">修改</a>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- /表格结束 -->
+    <!-- 分页开始 -->
+    <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+    <!-- /分页结束 -->
+    <!-- 模态框 -->
+    <el-dialog
+      title="录入栏目信息"
+      :visible.sync="visible"
+      width="60%">
+        ---{{form}}
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="栏目名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="序号">
+          <el-input  v-model="form.number"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button size="small" @click="closeModalHandler">取 消</el-button>
+        <el-button size="small" type="primary" @click="submitHandler">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- /模态框 -->
+
+  </div>
 </template>
+
+<script>
+import request from '@/utils/request'
+import querystring from 'querystring'
+export default {
+  // 用于存放网页中需要调用的方法
+  methods:{
+    loadData(){
+      let url = "http://localhost:6677/category/findAll"
+      request.get(url).then((response)=>{
+        // 将查询结果设置到categorys中，this指向外部函数的this
+        this.categorys = response.data;
+      })
+    },
+    submitHandler(){
+      let url = "http://localhost:6677/category/saveOrUpdate";
+      request({
+        url,
+        method:"POST",
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        data:querystring.stringify(this.form)
+      }).then((response)=>{
+        // 模态框关闭
+        this.closeModalHandler();
+        // 刷新
+        this.loadData();
+        // 提示消息
+        this.$message({
+          type:"success",
+          message:response.message
+        })
+      })
+    },
+    toDeleteHandler(id){
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      })
+      
+    },
+    toUpdateHandler(){
+      this.visible = true;
+    },
+    closeModalHandler(){
+      this.visible = false;
+    },
+    toAddHandler(){
+      this.visible = true;
+    }
+  },
+  // 用于存放要向网页中显示的数据
+  data(){
+    return {
+      visible:false,
+      categorys:[],
+      form:{
+        type:"category"
+      }
+    }
+  },
+  created(){
+    // this为当前vue实例对象r
+    // vue实例创建完毕 
+    this.loadData();
+
+  }
+}
+
+</script>
+
+<style scoped>
+ 
+</style>
